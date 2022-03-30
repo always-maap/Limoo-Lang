ee6fdebc40e7da94b07a5d0b8044768068293248
tur1ng <mohammadali.ap.2000@gmail.com>
2022-03-30 11:39:34 +0430

    chore(website): change default code sample

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -3,13 +3,23 @@ import '@codesandbox/sandpack-react/dist/index.css';
 import Container from '../components/Container';
 import TerminalViewer from '../components/TerminalViewer';
 
+const exampleCode = `let sum = fn(a, b) {
+  return a + b;
+}
+
+sum(5, 2)`;
+
 export default function Home() {
   return (
     <Container>
       <SandpackProvider
         customSetup={{
           entry: 'main.lp',
-          files: { 'main.lp': { code: 'let x = 5;' } },
+          files: {
+            'main.lp': {
+              code: exampleCode,
+            },
+          },
         }}
       >
         <SandpackLayout>
