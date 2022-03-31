2e96f875b150e8eb593e817c22a222b163136d5c
tur1ng <mohammadali.ap.2000@gmail.com>
2022-03-31 11:41:35 +0430

    chore: remove and ignore pkg

--- pkg/limoo.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {string} input
-* @returns {string}
-*/
-export function limoo_eval(input: string): string;
--- pkg/limoo.js
+++
@@ -1,2 +0,0 @@
-import * as wasm from "./limoo_bg.wasm";
-export * from "./limoo_bg.js";
